d4bac1716a6b5661d6efd989965d19b2ddb7ae71
Dylan Li <dylanli0419@gmail.com>
2025-03-26 16:27:09 -0400

    feat(ui); updated login button and graph description styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "MakeTheCut",
-  description: "McMaster Stream Statistics",
+  description: "McMaster Stream Predictions",
   icons: {
     icon: [
       { url: "/makethecut.ico", sizes: "any" }
--- frontend/components/HorizontalBarChart.tsx
+++ frontend/components/HorizontalBarChart.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 import { Bar, BarChart, CartesianGrid, ReferenceLine, XAxis, YAxis, Label, ResponsiveContainer, Cell, Tooltip as ChartTooltip } from "recharts";
 import { account, database } from "../app/appwrite";
+import { CardDescription } from "@/components/ui/card";
 import GradientPulse from "./GradientPulse";
 
 
@@ -165,6 +166,7 @@ export default function HorizontalBarChart() {
       <Card className="bg-neutral-900 text-white w-full md:w-2/3 mx-auto border-none p-5 pt-10 relative overflow-hidden">
         <CardHeader className="text-neutral-500">
           <CardTitle className="text-subtitle ">Loading Stream Data...</CardTitle>
+
         </CardHeader>
         <CardContent className="h-[500px] md:h-[600px] flex items-center justify-center">
           <div className="relative w-full h-full">
@@ -178,15 +180,20 @@ export default function HorizontalBarChart() {
   }
   
   return (
-    <Card className="bg-neutral-900 text-white w-full md:w-2/3 mx-auto border-none p-1 pt-10 pb-5 lg:pb-5 mb-20">
+    <Card className="bg-neutral-900 text-white w-full md:w-2/3 mx-auto border-none p-1 pt-10 pb-7 lg:pb-5 mb-20">
       <CardHeader className="text-neutral-500">
-        <CardTitle className="text-subtitle flex items-center justify-center gap-3">
+        <div className="flex flex-col justify-center items-center">
+        <CardTitle className="text-subtitle flex items-center gap-3 mb-1">
           <div className="relative w-3 h-3">
             <div className="absolute inset-0 rounded-full bg-red-500"></div>
             <div className="absolute inset-0 rounded-full bg-red-500 animate-ping opacity-75"></div>
           </div>
           Live Estimated Stream Cutoffs
         </CardTitle>
+        <CardDescription className="text-teenytiny">Data will become more accurate with more users</CardDescription>
+        </div>
+
+        
       </CardHeader>
       <CardContent className="h-[500px] md:h-[600px] pr-3 pl-3 md:pl-7">
         <ChartContainer config={chartConfig} className="h-full w-full">
@@ -196,8 +203,8 @@ export default function HorizontalBarChart() {
               data={chartData} 
               layout="vertical" 
               margin={isMobile ? 
-              { top: 30, right: 20, left: 5, bottom: 10 } : 
-              { top: 30, right: 30, left: 30, bottom: 10 }
+              { top: 30, right: 20, left: 5, bottom: 20 } : 
+              { top: 30, right: 30, left: 30, bottom: 20 }
               }
             >
               <CartesianGrid horizontal={false} stroke="#333" />
