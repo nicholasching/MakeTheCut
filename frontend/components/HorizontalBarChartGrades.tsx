b7b8fc6e8a5ac4c79547b4043d5f1d076b2c0413
Nicholas C <nicholaslching@gmail.com>
2025-03-28 02:00:57 -0400

    UI improvements

--- frontend/components/HorizontalBarChartGrades.tsx
+++ frontend/components/HorizontalBarChartGrades.tsx
@@ -181,6 +181,10 @@ export default function CourseGradeChart() {
               <div className="absolute inset-0 rounded-full bg-red-500 animate-ping opacity-75"></div>
             </div>
             Live - Reported Course Grade Averages
+            <div className="relative w-3 h-3">
+              <div className="absolute inset-0 rounded-full bg-red-500"></div>
+              <div className="absolute inset-0 rounded-full bg-red-500 animate-ping opacity-75"></div>
+            </div>
           </CardTitle>
           <CardDescription className="text-tiny flex md:flex-col items-center text-center font-semibold flex-col-reverse">
             <p>Current Contributions: {totalContributions}</p>
