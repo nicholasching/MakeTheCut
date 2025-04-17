a775eb31850b90cd32398eb493be8f7379ce13df
Nicholas C <nicholaslching@gmail.com>
2025-04-17 17:52:47 -0400

    feat: migrated to NYC servers

--- frontend/app/appwrite.js
+++ frontend/app/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account, Databases } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('67e081de000573c60d9f'); // Replace with your project ID
+    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    .setProject('makethecut'); // Replace with your project ID
 
 export const account = new Account(client);
 export const database = new Databases(client);
