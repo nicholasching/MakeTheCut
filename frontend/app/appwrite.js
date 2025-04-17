3adb87372fb4fa0fbce47500359ab98132ec108f
Nicholas C <nicholaslching@gmail.com>
2025-04-17 18:24:31 -0400

    feat: updated API endpoint to custom domain

--- frontend/app/appwrite.js
+++ frontend/app/appwrite.js
@@ -3,7 +3,7 @@ import { Client, Account, Databases } from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    .setEndpoint('https://appwrite.makethecut.ca/v1')
     .setProject('makethecut'); // Replace with your project ID
 
 export const account = new Account(client);
