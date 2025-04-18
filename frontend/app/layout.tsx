0f2ef0d1d0f92600dc569c68bd72208e638b9cfb
Nickchaching <nicholaslching@gmail.com>
2025-04-17 22:57:51 -0400

    feat: added temporary announcement banner

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
+        {/* Apply gradient background instead of glow */}
+        <div className="bg-gradient-to-r from-blue-800 via-orange-700 to-orange-900 text-primary-foreground text-center p-3 text-sm font-medium animate-glow">
+          Good News! MakeTheCut now works on McMaster Wifi.
+        </div>
         {children}
         <Analytics />
       </body>
