916166fadf7346addbd346e96fda1bf57fe91b14
Nickchaching <nicholaslching@gmail.com>
2025-06-27 00:06:27 -0400

    chore: updated attribution

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <footer className="w-full bg-neutral-950 border-t border-neutral-800 py-4 md:py-6 mt-auto flex justify-between px-10 md:px-30 gap-10">
       <div className="pt-2 md:text-left">
         <p className="text-neutral-500 text-tiny">
-        Made with ❤️ by McMaster Students / © {currentYear} MakeTheCut.
+        Made with ❤️ by <a href="https://www.nicholasching.ca" className="text-neutral-500 hover:text-red-500 transition-colors">Nicholas Ching</a> and <a href="https://www.dylanli.ca/" className="text-neutral-500 hover:text-red-500 transition-colors">Dylan Li</a> / © {currentYear} MakeTheCut.
         </p>
       </div>
       <div className="flex items-center gap-4">
