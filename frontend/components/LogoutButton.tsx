45c4df196ba6d89e1f095071b86d0c0cd24e6ee7
Nickchaching <nicholaslching@gmail.com>
2026-04-11 17:47:11 -0400

    chore: update nav button size

--- frontend/components/LogoutButton.tsx
+++ frontend/components/LogoutButton.tsx
@@ -32,10 +32,10 @@ const LogoutButton = ({
                         <DialogTrigger asChild>
                             <button
                                 type="button"
-                                className="flex size-12 cursor-pointer items-center justify-center md:size-10"
+                                className="flex max-md:size-8 cursor-pointer items-center justify-center md:h-7 md:w-7 lg:h-8 lg:w-8"
                                 aria-label="Account menu"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="white" className="size-full transition-colors duration-200 hover:fill-neutral-400 hover:scale-105">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="white" className="max-md:size-full transition-colors duration-200 hover:fill-neutral-400 hover:scale-105 md:min-w-[2rem] md:h-auto lg:min-w-[2rem] lg:w-[1.5vw]">
                                 <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/>
                                 </svg>
                             </button>
